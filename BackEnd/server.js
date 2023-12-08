87945fb3215c0f682de3b3810ebce7d92bb831c5
g00410404 <G00410404@atu.ie>
2023-12-08 14:37:19 +0000

    lab10 done

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,7 +3,12 @@ const app = express()
 const port = 4000
 const cors = require('cors');
 
+// Serve the static files from the React app
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('/static', express.static(path.join(__dirname, 'build//static')));
 
+// Enable CORS (Cross-Origin Resource Sharing)
 app.use(cors());
 app.use(function(req, res, next) {
 res.header("Access-Control-Allow-Origin", "*");
@@ -13,6 +18,8 @@ res.header("Access-Control-Allow-Headers",
 next();
 });
 
+
+// Parse incoming JSON requests
 const bodyParser = require("body-parser");
 
 //Here we are configuring express to use body-parser as middle-ware.
@@ -20,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-// getting-started.js
+// Connect to MongoDB using async/await
 const mongoose = require('mongoose');
 
 main().catch(err => console.log(err));
@@ -31,14 +38,17 @@ async function main() {
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
+// Define the schema for the 'book' collection
 const bookSchema = new mongoose.Schema({
   title:String,
   cover:String,
   author:String
 })
 
+// Create a model for the 'book' collection
 const bookModel = mongoose.model('sdfsdfsdfsdfsdfffffffffffff423', bookSchema);
 
+// Handle DELETE request to delete a book item by ID
 app.delete('/api/book/:id', async(req,res)=>{
   console.log("Delete: "+req.params.id);
 
@@ -47,6 +57,7 @@ app.delete('/api/book/:id', async(req,res)=>{
 
 })
 
+// Handle PUT request to update a book item by ID
 app.put('/api/book/:id', async(req, res)=>{
   console.log("Update: "+req.params.id);
 
@@ -54,7 +65,7 @@ app.put('/api/book/:id', async(req, res)=>{
   res.send(book);
 })
 
-
+// Handle POST request to create a new book item
 app.post('/api/book', (req,res)=>{
     console.log(req.body);
 
@@ -68,22 +79,29 @@ app.post('/api/book', (req,res)=>{
 
 })
 
+// Handle GET request for the root endpoint
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// Handle GET request to retrieve all book items
 app.get('/api/books', async(req, res)=>{
     
   let books = await bookModel.find({});
   res.json(books);
 })
-
+// Handle GET request to retrieve a specific food item by ID
 app.get('/api/book/:identifier',async (req,res)=>{
   console.log(req.params.identifier);
 
   let book = await bookModel.findById(req.params.identifier);
   res.send(book);
 })
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/../build/index.html'));
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
